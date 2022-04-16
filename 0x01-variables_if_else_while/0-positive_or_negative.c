e8ab911ba91e3eb710d14d57297fc98481893343
Eyakenojnr <eyakenojnr@gmail.com>
2022-04-16 06:49:23 -0700

    print if a number is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,12 +17,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 1)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if
+	{
 		printf("%d is negative\n", n);
+	}
 	else
-
+	{
 		printf("%d is zero\n", n);
+	}
 
 	return (0);
 }
