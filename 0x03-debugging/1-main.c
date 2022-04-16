2b5bf0896eceea81b76a433962c722898ea4bd5e
Eyakenojnr <eyakenojnr@gmail.com>
2022-04-15 19:46:45 -0700

    comment out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*
 	*while (i < 0)
 	*{
@@ -19,6 +20,5 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
