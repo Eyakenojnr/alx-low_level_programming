a2914df5b0fcc6367f249a40dc52821916647437
Eyakenojnr <eyakenojnr@gmail.com>
2022-04-20 20:12:55 -0700

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,9 @@ void print_rev(char *s)
 
 	for (index = 0; s[index] != '\0'; index++)
 		;
-	for (index = index - 1; s[index] != '\0'; index++)
+	for (index = index - 1; s[index] != '\0'; index--)
+	{
 		_putschar(s[index]);
+	}
 	_putschar('\n');
 }
