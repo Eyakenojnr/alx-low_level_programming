e431011120d45b60e7d8e31c927da4a0bb431558
Uduak Umanah <eyakenojnr@gmail.com>
2022-05-27 17:58:58 -0700

    insert new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,5 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		h->next = next;
 	}
+	else
+	{
+		new->next = h->next;
+		h->next = next;
+	}
 	return (new);
 }
