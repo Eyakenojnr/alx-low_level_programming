d4929335b5b38405dd5211d07c5a7aeb93427e49
Uduak Umanah <eyakenojnr@gmail.com>
2022-06-08 17:14:52 -0700

    encode a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * rot13 - encoding a string using rot13
- * @s: string to be encoded
- * Return: encoded string
+ * rot13 - function that encodes a string using rot13
+ *
+ * @s: the string to be encoded to rot13
+ *
+ * Return: the string s encoded to rot13.
  */
 
 char *rot13(char *s)
 {
 	int i = 0, j = 0;
-	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKk
-		LlMm";
-	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWw
-		XxYyZz";
+	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 	while (s[i] != '\0')
 	{
@@ -27,6 +27,5 @@ char *rot13(char *s)
 		j = 0;
 		i++;
 	}
-
 	return (s);
 }
