c5054e0da630d101e858488423a45812e780d222
Uduak Umanah <96398168+Eyakenojnr@users.noreply.github.com>
2022-07-13 07:16:17 +0100

    insert a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given 
- * position
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
  * @h: head of the list
  * @idx: index of the new node
