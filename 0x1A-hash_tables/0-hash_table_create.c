fc139502c0639c8bee0bed383f68aa66ad87ebfc
Eyakenojnr <eyakenojnr@gmail.com>
2022-07-30 20:05:40 -0700

    creates a hash table.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table (as name implies) of size
+ *  @size
+ *
+ * @size: The size of the hash table (the spaces allocated for nodes)
+ *
+ * Return: pointer to the hash table on success, else, null
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	if (size == 0)
+		return (NULL);
+
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	return (hash_table);
+}
